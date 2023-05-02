c01f0030b2fdf8ba6a0435502c094850bdd2ac51
Jan Menne <jmenne@uni-muenster.de>
2023-05-02 13:58:17 +0200

    Backend init

---
+++ src/api/routes.py
@@ -0,0 +1,34 @@
+from flask_restful import Api
+from flask_restful import Resource
+
+from src.resources import Exchange
+
+# initializing Flask API
+api = Api()
+
+
+class TestRoute(Resource):
+    """
+    Test route for the API.
+
+    method: GET
+    """
+
+    def get(self):  # dead: disable
+        """
+        Creates an Exchange instance and runs hello() method.
+
+        Args:
+            self
+
+        Returns:
+            "hello"
+
+        """
+        ex1 = Exchange()
+        return ex1.hello()
+
+
+# API endpoints
+
+api.add_resource(TestRoute, '/api/hello')
