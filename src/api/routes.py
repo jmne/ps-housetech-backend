4a092a4b51abf4af34b2c223ab6020d70e6fe4c4
Jan <jmenne@uni-muenster.de>
2023-05-11 18:34:24 +0200

    Add e-ink microcontroller api endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 from flask_restful import Resource
 
 from src.resources.bustracker import BusTracker
+from src.resources.einkgenerator import EInkGenerator
 
 # initializing Flask API
 api = Api()
@@ -28,6 +29,28 @@ class Bus(Resource):
         return BusTracker().get_future_rides()
 
 
+class EInk(Resource):
+    """
+    Test route for the API.
+
+    method: GET
+    """
+
+    def get(self):  # dead: disable
+        """
+        Creates an Exchange instance and runs hello() method.
+
+        Args:
+            self
+
+        Returns:
+            "hello"
+
+        """
+        return EInkGenerator().get_data()
+
+
 # API endpoints
 
 api.add_resource(Bus, '/api/bus')
+api.add_resource(EInk, '/api/eink')
