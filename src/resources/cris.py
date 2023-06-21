936e77e9a447a29fc46c0bcee2acccef57acc5b8
Florian Robrecht <frobrech@uni-muenster.de>
2023-06-21 13:12:22 +0200

    adapted cris response

--- src/resources/cris.py
+++ src/resources/cris.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 from .proxy_config import proxies
+# import xmltodict
 
 
 class CrisTracker:
@@ -20,6 +21,8 @@ class CrisTracker:
             self
         """
         self.url = 'https://cris-api-staging.uni-muenster.de/'
+        self.base_picture_url = '''https://www.uni-muenster.de/converis/
+                                    ws/public/infoobject/get/Picture/'''
         self.header = {
             'Accept-Encoding': 'gzip, deflate, br',
             'Content-Type': 'application/json',
@@ -127,43 +130,63 @@ class CrisTracker:
         TODO: if len(response) > 100:
         --> query again for the chair beginning with
         the endcursor of the last query.
-        TODO: there may be duplicates, not handled yet.
         """
         for chair in self.chairs:
-            payload = {
-                'query': f"""
-                                    query {{
-                                        organisation(id:{chair["chair_id"]}){{
-                                            connections{{
-                                                persons(first:100){{
-                                                    edges{{
-                                                        node{{
-                                                            id
-                                                            cfFirstNames
-                                                            cfFamilyNames
-                                                            status
-                                                        }}
-                                                    }}
+            request_is_necessary = True
+            paginator = ''
+            while request_is_necessary:
+                payload = {
+                    'query': f"""
+                            query {{
+                                organisation(id:{chair["chair_id"]}){{
+                                    connections{{
+                                        persons(first:100, after:"{paginator}"){{
+                                        pageInfo{{
+                                        startCursor
+                                        endCursor }}
+                                            edges{{
+                                                node{{
+                                                    id
+                                                    cfFirstNames
+                                                    cfFamilyNames
+                                                    status
                                                 }}
                                             }}
                                         }}
                                     }}
-                        """,
-            }
-            response = json.loads(
-                self.session.post(
-                    self.url, headers=self.header, json=payload,
-                ).text,
-            )
-            # print(response)
-            for employee in (
-                response['data']['organisation']
-                ['connections']['persons']['edges']
-            ):
-                self.employees.append({
-                    'id': employee['node']['id'],
-                    'chair': chair['chair_name'],
-                })
+                                }}
+                            }}
+                            """,
+                }
+                response = json.loads(
+                    self.session.post(
+                        self.url, headers=self.header, json=payload,
+                    ).text,
+                )
+
+                if len(
+                    response['data']['organisation']
+                    ['connections']['persons']['edges'],
+                ) != 100:
+                    request_is_necessary = False
+                else:
+                    paginator = (
+                        response['data']['organisation']
+                        ['connections']['persons']['pageInfo']['endCursor']
+                    )
+
+                for employee in (
+                    response['data']['organisation']
+                    ['connections']['persons']['edges']
+                ):
+                    # only people with status 3 => active
+                    if employee['node']['status'] != 3:
+                        continue
+
+                    self.employees.append({
+                        'id': employee['node']['id'],
+                        'chair': chair['chair_name'],
+                    })
 
         return
 
@@ -215,42 +238,44 @@ class CrisTracker:
                                 }}
                             """,
             }
-            response = json.loads(
-                self.session.post(
-                    self.url, headers=self.header, json=payload,
-                ).text,
-            )
-            for index, (chair, employee_data) in enumerate(zip(lochairs, response['data']['nodes'])):  # noqa: E501
-                for edge in response['data']['nodes'][index]['connections']['cards']['edges']:  # noqa: E501
-                    # hier logik abändern
-                    if (
-                        edge['node']['email'] is None
-                        or edge['node']['email'] is None
-                    ):
-                        continue
-                    else:
-                        try:
-                            self.result.append({
-                                'academicTitle': employee_data['node']['academicTitle'],
-                                'cfFirstNames': employee_data['node']['cfFirstNames'],
-                                'cfFamilyNames': employee_data['node']['cfFamilyNames'],
-                                'roomNumber':  edge['node']['roomNumber'],
-                                'email': edge['node']['email'],
-                                'phone': edge['node']['phone'],
-                                'chair': chair,
-                                'image':
-                                employee_data['connections']['pictures']['edges'][0]['node']['id'],  # noqa: E501
+            response = {
+                'persons': json.loads(
+                    self.session.post(
+                        self.url, headers=self.header, json=payload,
+                    ).text,
+                ),
+                'chairs': lochairs,
+            }
 
-                            })
-                            break
-                        except IndexError as e:
-                            print(
-                                employee_data['node']['cfFirstNames'],
-                                employee_data['node']['cfFamilyNames'],
-                            )
-                            print(e)
-                            break
+            persons = response['persons']['data']['nodes']
+            for chair, person in zip(response['chairs'], persons):
+                emails = []
+                phones = []
+                roomNumber = None
+                for edge in person['connections']['cards']['edges']:
+                    email = edge['node']['email']
+                    phone = edge['node']['phone']
+                    if email not in emails and email is not None:
+                        emails.append(email)
+                    if phone not in phones and phone is not None:
+                        phones.append(phone)
+                    if roomNumber is None and edge['node']['roomNumber'] is not None:
+                        roomNumber = edge['node']['roomNumber']
 
+                picture_id = None
+                if person['connections']['pictures']['edges']:  # noqa: 501
+                    picture_id = person['connections']['pictures']['edges'][0]['node']['id']  # noqa: 501
+
+                self.result.append({
+                    'academicTitle': person['node']['academicTitle'],
+                    'cfFirstNames': person['node']['cfFirstNames'],
+                    'cfFamilyNames': person['node']['cfFamilyNames'],
+                    'roomNumber': roomNumber,
+                    'emails': emails,
+                    'phones': phones,
+                    'chair': chair,
+                    'image': picture_id,
+                })
         return
 
     def remove_duplicate_employees(self):
@@ -266,27 +291,42 @@ class CrisTracker:
         """Function that adds addresses for every employee.
 
         Address depends on the chair the person is working in.
+        TODO: Outsource the chair address matching to config file
         """
-        pass
+        for card in self.result:
+            if 'Prof. Klein' in card['chair'] or 'Prof. Berger' in card['chair']:
+                card['address'] = 'Leonardo-Campus 11'
+            else:
+                card['address'] = 'Leonardo-Campus 3'
+        return
+
+    '''
+    def add_pictures(self):
+        """Function that adds picture base 64 blob for every employee.
+
+        The function overwrites the image value with the base 64 blob.
+        """
+        for card in self.result:
+            if card['image'] is None:
+                continue
+            response = self.session.get(
+                f'{self.base_picture_url}{str(card["image"])}',
+            )
+            if response.status_code != 200:
+                card['image'] = None
+                continue
+            response_list = xmltodict.parse(response.text)
+            for attr in response_list['infoObject']['attribute']:
+                if attr['@name'] != 'File data':
+                    continue
+                card['image'] = attr['data']'''
 
     def get_cris_data(self):
         """Function that returns the desired result."""
         self.update_employees()
-        print(len(self.employees))
         self.employees = self.remove_duplicate_employees()
         print(len(self.employees))
         self.update_result()
         self.add_addresses()
+        # self.add_pictures()
         return self.result
-
-
-if __name__ == '__main__':
-    cristracker = CrisTracker()
-    print(cristracker.get_cris_data())
-    '''
-    cristracker.update_chairs()
-    print(cristracker.chairs)
-    cristracker.update_employees()
-    cristracker.update_result()
-    print(cristracker.result, len(cristracker.result))
-    print(len(cristracker.employees))'''
