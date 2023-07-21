285e0f0e08e4cac618100f723101185705635653
kschulz6@uni-muenster.de <kschulz6@uni-muenster.de>
2023-07-21 13:12:04 +0200

    tried again

--- tests/test_instagram.py
+++ tests/test_instagram.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 def test_instagram(client):
     """
     The function checks the following test cases.
@@ -6,38 +8,38 @@ def test_instagram(client):
     2. Verify that the returned data is a dictionary
     3. Verify that the post_data dictionary contains all the expected keys
     4. Verify that the latest_posts data is a list of dictionaries
-    5. Verify that the latest_posts dictionary contains all the expected keys
-    6. Check if the latest posts are within the last 30 days
+    5. Verify that each dictionary in latest_posts contains all the expected keys
+    6. Check if the timestamp of returned posts is within the last 30 days
     """
 
-    # define sample data
+    # Define sample data
     post_data_sample = {
-    'id': '1234567890',
-    'media_type': 'IMAGE',
-    'media_url': 'https://example.com/image.jpg',
-    'username': 'test_user',
-    'timestamp': '2023-07-20T12:34:56Z',
+        'id': '1234567890',
+        'media_type': 'IMAGE',
+        'media_url': 'https://example.com/image.jpg',
+        'username': 'test_user',
+        'timestamp': '2023-07-20T12:34:56Z',
     }
 
     latest_posts_sample = [
-    {
-        'caption': 'Post 1',
-        'media_url': 'https://example.com/post1.jpg',
-        'media_type': 'IMAGE',
-        'timestamp': '2023-07-20T12:34:56Z',
-    },
-    {
-        'caption': 'Post 2',
-        'media_url': 'https://example.com/post2.jpg',
-        'media_type': 'VIDEO',
-        'timestamp': '2023-07-19T10:22:34Z',
-    },
-    {
-        'caption': 'Post 3',
-        'media_url': 'https://example.com/post3.jpg',
-        'media_type': 'CAROUSEL_ALBUM',
-        'timestamp': '2023-07-18T15:45:12Z',
-    },
+        {
+            'caption': 'Post 1',
+            'media_url': 'https://example.com/post1.jpg',
+            'media_type': 'IMAGE',
+            'timestamp': '2023-07-20T12:34:56Z',
+        },
+        {
+            'caption': 'Post 2',
+            'media_url': 'https://example.com/post2.jpg',
+            'media_type': 'VIDEO',
+            'timestamp': '2023-07-19T10:22:34Z',
+        },
+        {
+            'caption': 'Post 3',
+            'media_url': 'https://example.com/post3.jpg',
+            'media_type': 'CAROUSEL_ALBUM',
+            'timestamp': '2023-07-18T15:45:12Z',
+        },
     ]
 
     # Define the expected keys for the latest_posts dictionary
@@ -46,20 +48,15 @@ def test_instagram(client):
         'media_type', 'timestamp',
     }
 
- 
-
-
     response = client.get('/api/bus')
     data = response.get_json()
-
 
     # Test the response status code
     assert response.status_code == 200
 
-
     # Test get_post_data method with sample data
     post_data = post_data_sample
-     # Define the expected keys for the post_data dictionary
+    # Define the expected keys for the post_data dictionary
     expected_keys = [
         'id', 'media_type',
         'media_url', 'username',
@@ -70,19 +67,16 @@ def test_instagram(client):
     # Verify that the post_data dictionary contains all the expected keys
     assert all(key in post_data for key in expected_keys)
     
-    
-    # Define the expected keys for the latest_posts dictionary
-    item_keys = {
-        'caption', 'media_url',
-        'media_type', 'timestamp',
-    }
-    # Test get_post_data method with sample data
+    # Test get_latest_posts method with sample data
     latest_posts = latest_posts_sample
-    # verify that the returned data is a list of dictionaries.
+    # Verify that the returned data is a list of dictionaries.
     assert isinstance(latest_posts, list)
     assert all(isinstance(post, dict) for post in latest_posts)
-    # verify that each post dictionary contains all the expected keys
-    assert all(key in post for key in item_keys)
-    # check if the timestamp of returned posts is within the last 30 days
+    
+    # Verify that each post dictionary contains all the expected keys
+    for post in latest_posts:
+        assert all(key in post for key in item_keys)
+
+    # Check if the timestamp of returned posts is within the last 30 days
     assert all(isinstance(datetime.fromisoformat(post['timestamp'][:-1]), datetime) for post in latest_posts)
     assert all(datetime.now() - timedelta(days=30) <= datetime.fromisoformat(post['timestamp'][:-1]) <= datetime.now() for post in latest_posts)
