9f028b4bf5d355c4b6d9241d29e6172f8a2d82d9
Jan <jmenne@uni-muenster.de>
2023-07-25 20:22:10 +0200

    Fix bug RGBA mode Pillow

--- src/resources/picture.py
+++ src/resources/picture.py
@@ -38,6 +38,8 @@ class PictureTracker(Tracker):
             new_width = 1000
             new_height = int(new_width * aspect_ratio)
             image = image.resize((new_width, new_height), Image.LANCZOS)
+            if image.mode in ('RGBA', 'P'):
+                image = image.convert('RGB')
             byte_arr = BytesIO()
             image.save(byte_arr, format='JPEG', optimize=True, quality=90)
             response = make_response(byte_arr.getvalue())
