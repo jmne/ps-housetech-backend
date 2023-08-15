9f24454bce4046fce50198d3c49fa2677fee7a47
Florian Finn Robrecht <frobrech@uni-muenster.de>
2023-08-15 11:32:07 +0200

    Resolve "Improve error handling for all modules"

--- tests/test_mensa.py
+++ tests/test_mensa.py
@@ -13,24 +13,28 @@ def test_mensa(client):
     for language in languages:
         for mensa in cafeterias:
             response = client.get(f'/api/mensa/{mensa}/{language}')
+            print(response)
             data = response.get_json()
             # Test the response status code
-            assert response.status_code == 200
-            # Test the response contains a list of dictionaries
-            assert isinstance(data, list)
+            if response.status_code == 302:
+                continue
+            elif response.status_code == 200:
 
-            # Test each dictionary contains the expected keys
-            expected_keys = {
-                'date', 'weekday', 'item',
-            }
-            for item in data:
-                assert set(item.keys()) == expected_keys
+                # Test the response contains a list of dictionaries
+                assert isinstance(data, list)
 
-            # Test each item in the "item" list contains the expected keys
-            item_keys = {
-                'meal', 'foodicons',
-                'price1', 'price3',
-            }
-            for day in data:
-                for meal in day['item']:
-                    assert set(meal.keys()) == item_keys
+                # Test each dictionary contains the expected keys
+                expected_keys = {
+                    'date', 'weekday', 'item',
+                }
+                for item in data:
+                    assert set(item.keys()) == expected_keys
+
+                # Test each item in the "item" list contains the expected keys
+                item_keys = {
+                    'meal', 'foodicons',
+                    'price1', 'price3',
+                }
+                for day in data:
+                    for meal in day['item']:
+                        assert set(meal.keys()) == item_keys
